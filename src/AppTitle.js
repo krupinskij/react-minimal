f96ca097baf007f51bfe9735a23f62fc9895fd05
krupinskij <jk@op.pl>
2019-10-08 18:02:51 +0200

    Lab1Ex1

---
+++ src/AppTitle.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppTitle = () => (
+    <h1>Minimal React krupinskij</h1>
+)
+
+export default AppTitle
--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import AppTitle from './AppTitle';
 
 const MyApp = () => (
   <div>
-    <h1>Minimal React krupinskij</h1>
+    <AppTitle />
     <p>Bundle size: 91 bytes, Load time of the bundle: 44 ms, Last commit SHA1: b6347b74590b7816a799cf0c5322fef5fc54aa66</p>
   </div>
 )
