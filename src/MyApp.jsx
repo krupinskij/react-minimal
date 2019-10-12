143a0a073e6f95c36a473c40c1a09fbe3101b7ac
krupinskij <jk@op.pl>
2019-10-12 21:41:09 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import AppTitle from './AppTitle';
 
+const generateArray = (n) => { return Array.from(Array(n), (e,i)=>i+1) };
+
 const MyApp = () => (
   <div>
     <AppTitle />
