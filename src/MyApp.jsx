f07177617246afb76016afb340de202a8ae6ecad
krupinskij <jk@op.pl>
2019-10-12 22:07:14 +0200

    Lab1Ex4

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -5,10 +5,15 @@ const generateArray = (n) => { return Array.from(Array(n), (e,i)=>i+1) };
 
 const generateRandomArray = (n) => { return Array.from(Array(n), (e)=>Math.floor(Math.random()*25)+1) };
 
+const arr1 = [2, 56, 23, 88, 17, 4];
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <p>Bundle size: 91 bytes, Load time of the bundle: 44 ms, Last commit SHA1: b6347b74590b7816a799cf0c5322fef5fc54aa66</p>
+    <p>{ generateArray(13).toString() }</p>
+    <p>{ generateRandomArray(15).toString() }</p>
+    <p>{ arr1.filter(x => x>15).toString() }</p>
   </div>
 )
 
